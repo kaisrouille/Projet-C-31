7d5bda1999307d11b4d8ea83bcd6b4e47c51de2e
jojojoj94 <test@LAPTOP-ODI3H59O>
2023-08-31 15:59:30 +0200

    avancee du menu

--- main.c
+++ main.c
@@ -11,7 +11,42 @@ typedef struct
 
 int main(int argc, char *argv[])
 {
-    printf("\n-----INCENDIE-----\n\n")
+    printf("\n-----INCENDIE-----\n\n");
+
+
+    printf("\nentrez le nombre de cellules en longueur : ");
+    int length;
+    scanf("%d", &length);
+    if(length < 1 || length > 20)
+    {
+        printf("\n\nEntree incorrecte : la longueur doit etre comprise entre 1 et 20\n\n");
+    }
+
+    printf("\nentrez le nombre de cellules en largeur : ");
+    int width;
+    scanf("%d", &width);
+    if(width < 1 || width > 20)
+    {
+        printf("\n\nEntree incorrecte : la largeur doit etre comprise entre 1 et 20\n\n");
+    }
+
+
+    entity matrice[length][width];
+
+    printf("Mode de jeu :\n\n1 - Manuel\n2- Automatique\n");
+
+    if(getchar() == '1')
+    {
+        int i, j;
+        for(i = 0; i < length; i++)
+        {
+            for(j = 0; j < width; j++)
+            {
+                printf("\n\nEntrez le type de la cellule %d %d : ", i, j);
+                scanf("%d", &matrice[i][j].type);
+                matrice[i][j].etat = 0;
+            }
+        }
 
 
 
@@ -23,15 +58,15 @@ int main(int argc, char *argv[])
 
 
 
-    entity matrice[][]
+    }
+    else if (getchar() == '2')
+    {
+        
 
 
 
 
-
-
-
-
+    }
 
     return 0;
 }
