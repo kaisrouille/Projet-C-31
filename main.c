53e738a3b955db30fa0cec65d202706e6efe6b0f
Lancina <ouatsonbg@gmail.com>
2023-08-31 17:06:31 +0200

    test2

--- main.c
+++ main.c
@@ -1,66 +1,66 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-typedef struct {
+typedef struct
+{
     char name[10];
     int type;
     int etat;
 } entity;
 
-
-int main(int argc, char *argv[])
+int main()
 {
-    printf("\n-----INCENDIE-----\n\n");
+    printf("\n=========================INCENDIE========================\n\n");
     int length;
     int width;
 
-    do {
-        printf("Entrez le nombre de cellules en longueur compris entre 1 et 20: ");
+    do
+    {
+        printf("Entrez le nombre de cellules en longueur de la foret, compris entre 1 et 20: ");
         scanf("%d", &length);
     } while (length < 1 || length > 20);
 
-    do {
-        printf("Entrez le nombre de cellules en largeur compris entre 1 et 20: ");
+    do
+    {
+        printf("Entrez le nombre de cellules en largeur de la foret, compris entre 1 et 20: ");
         scanf("%d", &width);
     } while (width < 1 || width > 20);
 
-
-    entity matrice[length][width];
+    entity **matrice = malloc(length * sizeof(entity *));
+    if (matrice == NULL)
+    {
+        printf("Echec de l'allocation\n");
+        return EXIT_FAILURE;
+    }
+    for (int i = 0; i < length; i++)
+    {
+        matrice[i] = malloc(width * sizeof(entity));
+        if (matrice[i] == NULL)
+        {
+            printf("Echec de l'allocation\n");
+            return EXIT_FAILURE;
+        }
+    }
 
     printf("Mode de jeu :\n\n1 - Manuel\n2- Automatique\n");
 
-    if(getchar() == '1')
+    char mode;
+    scanf(" %c", &mode);
+
+    if (mode == '1')
     {
-        int i, j;
-        for(i = 0; i < length; i++)
+        for (int i = 0; i < length; i++)
         {
-            for(j = 0; j < width; j++)
+            for (int j = 0; j < width; j++)
             {
                 printf("\n\nEntrez le type de la cellule %d %d : ", i, j);
-                scanf("%d", &matrice[i][j].type);
+                scanf("%d", &(matrice[i][j].type));
                 matrice[i][j].etat = 0;
             }
         }
-
-
-
-
-
-
-
-
-
-
-
     }
-    else if (getchar() == '2')
+    else if (mode == '2')
     {
-        
-
-
-
-
     }
-
     return 0;
 }
