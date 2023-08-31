bdb8e44cfd026886fe2884f2e84cf698bddba6e6
jojojoj94 <test@LAPTOP-ODI3H59O>
2023-08-31 15:18:47 +0200

    test1

---
+++ main.c
@@ -0,0 +1,37 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+typedef struct
+{
+    char name[10];
+    int type;
+    int etat;
+} entity;
+
+
+int main(int argc, char *argv[])
+{
+    printf("\n-----INCENDIE-----\n\n")
+
+
+
+
+
+
+
+
+
+
+
+    entity matrice[][]
+
+
+
+
+
+
+
+
+
+    return 0;
+}
