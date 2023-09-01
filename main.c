240d2770e51836f73aa6a05afbc5ab65e8819645
Lancina <ouatsonbg@gmail.com>
2023-09-01 10:37:33 +0200

    test4

--- main.c
+++ main.c
@@ -3,8 +3,8 @@
 
 typedef struct
 {
+    char symbole;
     char name[10];
-    int type;
     int degres;
     int etat;
 } entity;
@@ -60,7 +60,7 @@ int main()
             for (int j = 0; j < width; j++)
             {
                 printf("\n\nEntrez le type de la cellule %d %d : ", i, j);
-                scanf("%d", &(matrice[i][j].type));
+                scanf("%d", &(matrice[i][j].symbole));
                 matrice[i][j].etat = 0;
             }
         }
